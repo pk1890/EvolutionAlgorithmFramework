08976b37f8e84c2f7848821597bdb35f035a8cb9
KipinskiPiotr <kipinski@student.agh.edu.pl>
2019-12-04 23:56:44 +0100

    Added RandomTournamentSelector

---
+++ src/genetics/operators/RandomTournamentSelector.java
@@ -0,0 +1,44 @@
+package genetics.operators;
+
+import genetics.genes.Gene;
+import genetics.genes.Genotype;
+import genetics.genes.Population;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomTournamentSelector<G extends Gene> extends Selector<G>{
+    private int tournamentSize;
+    private int desiredPopulationSize;
+    private Random random = new Random();
+
+    @Override
+    public Population<G> select(Population<G> population) {
+        Population<G> selectedPopulation = new Population<G>();
+        while(selectedPopulation.getIndividuals().size() != desiredPopulationSize){
+            selectedPopulation.getIndividuals().add(playTournament(population));
+        }
+        return selectedPopulation;
+    }
+
+    public RandomTournamentSelector(int tournamentSize, int desiredPopulationSize){
+        this.tournamentSize = tournamentSize;
+        this.desiredPopulationSize = desiredPopulationSize;
+    }
+
+    private Genotype<G> playTournament(Population<G> population){
+        List<Genotype<G>> candidates = population.getIndividuals();
+        List<Genotype<G>> fighters = new ArrayList<>();
+        for(int i = 0; i < tournamentSize; i++){
+            fighters.add(candidates.get(random.nextInt(candidates.size())));
+        }
+        Genotype<G> bestFighter = fighters.get(0);
+        for(Genotype<G> g : fighters){
+            if(g.getFitness() > bestFighter.getFitness()){
+                bestFighter = g;
+            }
+        }
+        return bestFighter;
+    }
+}
